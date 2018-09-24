e8f3a2f7e9cead95eccb9149289dad31626b6d97
franck <carnet.franck.paul@gmail.com>
2018-09-24 19:02:45 +0200

    Use let and const rather than var (ES2015/ES6), rewrite promise

--- js/service.js
+++ js/service.js
@@ -2,124 +2,140 @@
 'use strict';
 
 dotclear.dmScheduledPostsCount = function() {
-  var params = {
-    f: 'dmScheduledPostsCount',
-    xd_check: dotclear.nonce,
-  };
-  $.get('services.php', params, function(data) {
-    if ($('rsp[status=failed]', data).length > 0) {
-      // For debugging purpose only:
-      // console.log($('rsp',data).attr('message'));
-      window.console.log('Dotclear REST server error');
-    } else {
-      var nb = $('rsp>count', data).attr('ret');
-      if (nb != dotclear.dbScheduledPostsCount_Counter) {
-        // First pass or counter changed
-        var icon = $('#dashboard-main #icons p a[href="posts.php?status=-1"]');
-        if (icon.length) {
-          // Update count if exists
-          var nb_label = icon.children('span.db-icon-title-dm-scheduled');
-          if (nb_label.length) {
-            nb_label.text(nb);
-          }
-        } else {
-          if (nb != '') {
-            // Add full element (link + counter)
-            icon = $('#dashboard-main #icons p a[href="posts.php"]');
-            if (icon.length) {
-              var xml = ' <a href="posts.php?status=-1"><span class="db-icon-title-dm-scheduled">' + nb + '</span></a>';
-              icon.after(xml);
+  $.get('services.php', {
+      f: 'dmScheduledPostsCount',
+      xd_check: dotclear.nonce,
+    })
+    .done(function(data) {
+      if ($('rsp[status=failed]', data).length > 0) {
+        // For debugging purpose only:
+        // console.log($('rsp',data).attr('message'));
+        window.console.log('Dotclear REST server error');
+      } else {
+        const nb = $('rsp>count', data).attr('ret');
+        if (nb != dotclear.dbScheduledPostsCount_Counter) {
+          // First pass or counter changed
+          let icon = $('#dashboard-main #icons p a[href="posts.php?status=-1"]');
+          if (icon.length) {
+            // Update count if exists
+            const nb_label = icon.children('span.db-icon-title-dm-scheduled');
+            if (nb_label.length) {
+              nb_label.text(nb);
+            }
+          } else {
+            if (nb != '') {
+              // Add full element (link + counter)
+              icon = $('#dashboard-main #icons p a[href="posts.php"]');
+              if (icon.length) {
+                const xml = ` <a href="posts.php?status=-1"><span class="db-icon-title-dm-scheduled">${nb}</span></a>`;
+                icon.after(xml);
+              }
             }
           }
+          // Store current counter
+          dotclear.dbScheduledPostsCount_Counter = nb;
         }
-        // Store current counter
-        dotclear.dbScheduledPostsCount_Counter = nb;
       }
-    }
-  });
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      window.console.log(`AJAX ${textStatus} (status: ${jqXHR.status} ${errorThrown})`);
+    })
+    .always(function() {
+      // Nothing here
+    });
 };
 
 dotclear.dmScheduledCheck = function() {
-  var params = {
-    f: 'dmScheduledCheck',
-    xd_check: dotclear.nonce
-  };
-  $.get('services.php', params, function(data) {
-    if ($('rsp[status=failed]', data).length > 0) {
-      // For debugging purpose only:
-      // console.log($('rsp',data).attr('message'));
-      window.console.log('Dotclear REST server error');
-    } else {
-      // Refresh list of last scheduled posts
-      var args = {
-        f: 'dmLastScheduledRows',
-        xd_check: dotclear.nonce
-      };
-      $.get('services.php', args, function(data) {
-        if ($('rsp[status=failed]', data).length > 0) {
-          // For debugging purpose only:
-          // console.log($('rsp',data).attr('message'));
-          window.console.log('Dotclear REST server error');
-        } else {
-          var xml = $('rsp>rows', data).attr('list');
-          // Replace current list with the new one
-          if ($('#scheduled-posts ul').length) {
-            $('#scheduled-posts ul').remove();
-          }
-          if ($('#scheduled-posts p').length) {
-            $('#scheduled-posts p').remove();
-          }
-          // Add current hour in badge on module
-          var now = new Date();
-          var time = now.toLocaleTimeString();
-          // Display module content
-          $('#scheduled-posts h3').after(xml);
-          // Display badge with current time
-          dotclear.badge(
-            $('#scheduled-posts'), {
-              id: 'dmsp',
-              value: time,
-              type: 'info'
+  $.get('services.php', {
+      f: 'dmScheduledCheck',
+      xd_check: dotclear.nonce
+    })
+    .done(function(data) {
+      if ($('rsp[status=failed]', data).length > 0) {
+        // For debugging purpose only:
+        // console.log($('rsp',data).attr('message'));
+        window.console.log('Dotclear REST server error');
+      } else {
+        // Refresh list of last scheduled posts
+        $.get('services.php', {
+            f: 'dmLastScheduledRows',
+            xd_check: dotclear.nonce
+          })
+          .done(function(data) {
+            if ($('rsp[status=failed]', data).length > 0) {
+              // For debugging purpose only:
+              // console.log($('rsp',data).attr('message'));
+              window.console.log('Dotclear REST server error');
+            } else {
+              const xml = $('rsp>rows', data).attr('list');
+              // Replace current list with the new one
+              if ($('#scheduled-posts ul').length) {
+                $('#scheduled-posts ul').remove();
+              }
+              if ($('#scheduled-posts p').length) {
+                $('#scheduled-posts p').remove();
+              }
+              // Add current hour in badge on module
+              const now = new Date();
+              const time = now.toLocaleTimeString();
+              // Display module content
+              $('#scheduled-posts h3').after(xml);
+              // Display badge with current time
+              dotclear.badge(
+                $('#scheduled-posts'), {
+                  id: 'dmsp',
+                  value: time,
+                  type: 'info'
+                }
+              );
             }
-          );
-        }
-      });
-    }
-  });
+          })
+          .fail(function(jqXHR, textStatus, errorThrown) {
+            window.console.log(`AJAX ${textStatus} (status: ${jqXHR.status} ${errorThrown})`);
+          })
+          .always(function() {
+            // Nothing here
+          });
+      }
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      window.console.log(`AJAX ${textStatus} (status: ${jqXHR.status} ${errorThrown})`);
+    })
+    .always(function() {
+      // Nothing here
+    });
 };
 
-dotclear.dmScheduledPostsView = function(line, action) {
+dotclear.dmScheduledPostsView = function(line, action, e) {
   action = action || 'toggle';
-  var id = $(line).attr('id').substr(4);
-  var li = document.getElementById('dmspe' + id);
-  if (!li && (action == 'toggle' || action == 'open')) {
-    li = document.createElement('li');
-    li.id = 'dmspe' + id;
-    li.className = 'expand';
+  if ($(line).attr('id') == undefined) {
+    return;
+  }
+
+  const postId = $(line).attr('id').substr(4);
+  const lineId = `dmspe${postId}`;
+  let li = document.getElementById(lineId);
+
+  if (!li) {
     // Get content
-    $.get('services.php', {
-      f: 'getPostById',
-      id: id,
-      post_type: ''
-    }, function(data) {
-      var rsp = $(data).children('rsp')[0];
-      if (rsp.attributes[0].value == 'ok') {
-        var content = $(rsp).find('post_display_excerpt').text() + ' ' + $(rsp).find('post_display_content').text();
-        if (content) {
-          $(li).append(content);
-        }
+    dotclear.getEntryContent(postId, function(content) {
+      if (content) {
+        li = document.createElement('li');
+        li.id = lineId;
+        li.className = 'expand';
+        $(li).append(content);
+        $(line).addClass('expand');
+        line.parentNode.insertBefore(li, line.nextSibling);
       } else {
-        window.alert($(rsp).find('message').text());
+        $(line).toggleClass('expand');
       }
+    }, {
+      clean: (e.metaKey),
+      length: 300
     });
+  } else {
+    $(li).toggle();
     $(line).toggleClass('expand');
-    line.parentNode.insertBefore(li, line.nextSibling);
-  } else if (li && li.style.display == 'none' && (action == 'toggle' || action == 'open')) {
-    $(li).css('display', 'block');
-    $(line).addClass('expand');
-  } else if (li && li.style.display != 'none' && (action == 'toggle' || action == 'close')) {
-    $(li).css('display', 'none');
-    $(line).removeClass('expand');
   }
 };
 
@@ -134,7 +150,7 @@ $(function() {
     dotclear.dmScheduled_Timer = setInterval(dotclear.dmScheduledCheck, 60 * 5 * 1000);
   }
   if (dotclear.dmScheduled_Counter) {
-    var icon = $('#dashboard-main #icons p a[href="posts.php"]');
+    const icon = $('#dashboard-main #icons p a[href="posts.php"]');
     if (icon.length) {
       // Icon exists on dashboard
       // First pass
