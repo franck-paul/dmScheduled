230cb7e626168f02b2ae00f970c7f8e2b78de22e
franck <carnet.franck.paul@gmail.com>
2024-09-05 07:02:49 +0200

    New release 4.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Scheduled Dashboard Module',
     'Display scheduled posts on dashboard',
     'Franck Paul',
-    '4.2',
+    '4.3',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
