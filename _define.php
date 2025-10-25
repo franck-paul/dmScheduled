732c93fc43966a2e1d1c85396f326126c1227078
franck <carnet.franck.paul@gmail.com>
2025-10-25 10:34:00 +0200

    New release 8.3

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Scheduled Dashboard Module',
     'Display scheduled posts on dashboard',
     'Franck Paul',
-    '8.2',
+    '8.3',
     [
-        'date'     => '2025-09-22T10:32:35+0200',
+        'date'     => '2025-10-25T10:34:00+0200',
         'requires' => [
             ['core', '2.36'],
             ['dmHelper', '5.0'],
